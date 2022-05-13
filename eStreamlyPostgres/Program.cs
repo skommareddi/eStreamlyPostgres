8c2a898e11a7ec553393f11311cea00bda4a2fd8
Raj S <39525002+rajkumars22@users.noreply.github.com>
2022-05-13 21:37:17 +0530

    initital commit

---
+++ eStreamlyPostgres/Program.cs
@@ -0,0 +1,39 @@
+﻿using DbUp;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace eStreamlyPostgres
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            var connectionString = args.FirstOrDefault() ?? "User ID=postgres;Password=admin@123;Host=localhost;Port=5432;Database=estreamly;Pooling=true;Integrated Security=true;";
+
+            var upgrader = DeployChanges.To
+                    .PostgresqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
---
+++ eStreamlyPostgres/Scripts/Script0001 - User Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE IF NOT EXISTS public."Address_Type_1test" ("Address_Type_Id_1test" integer NOT NULL,
+"Address_Type_Description" character varying NOT NULL,
+"Created_Date" timestamp with time zone NOT NULL,
+"Created_By" character varying NOT NULL,
+"Modified_Date" timestamp with time zone NULL,
+"Modified_By" character varying NULL,
+"Record_Version" numeric NOT NULL);
